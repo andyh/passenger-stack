65c8949002ffc2f03edebf87513bac27d192d7b7
Andy Henson <andrew.henson@foxsoft.net>
2009-03-25 12:38:02 +0000

    Updated to be more generic in mailserver usage and added mailutils as requirement

--- config/stack/mailserver.rb
+++ config/stack/mailserver.rb
@@ -1,12 +1,18 @@
-package :mailserver do
+package :postfix, :provides => :mailserver do
   description 'Postfix Mailserver'
   apt 'postfix' do
     pre :install, 'apt-get update'
   end
 
   verify do
-    has_executable 'master'
+    has_file '/usr/lib/postfix/master'
   end
   
   requires :build_essential
+  requires :mailutils
+end
+
+package :mailutils do
+  description 'Mail utilities'
+  apt 'mailutils'
 end
